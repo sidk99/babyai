135a03fc99223af4ef932b947ab6d01b743311fb
Ubuntu <ubuntu@ip-172-31-11-6.us-west-2.compute.internal>
2020-09-29 09:26:31 +0000

    prev mg on small aws

---
+++ babyai/levels/__init__.py
@@ -0,0 +1,11 @@
+from collections import OrderedDict
+
+from . import iclr19_levels
+from . import bonus_levels
+from . import test_levels
+from . import hrl_levels
+from . import hrl_transfer_levels
+from . import invar_levels
+from . import common_levels
+
+from .levelgen import test, level_dict
